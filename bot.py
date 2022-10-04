7857756e786f6172a1849c67f2e79c15f96398a9
Антон <Frensis001@gmail.com>
2022-10-04 17:00:10 +0500

    Вставил нормальный токен

--- bot.py
+++ bot.py
@@ -1,19 +1,14 @@
 import telebot
-import os
 
-TOKEN_TELEBOT = os.environ["TOKEN_TELEBOT"]
+token = '5719352624:AAE91fHqkvC_tT9fnKmAQIZyy_9jc07u6_k'
 
+bot = telebot.TeleBot(token)
 
-bot = telebot.TeleBot(TOKEN_TELEBOT)
-
-IS_HEROKU = os.environ.get('IS_HEROKU', False)
 
 @bot.message_handler(commands=['start'])
 def start_cmd(message):
-    if IS_HEROKU:
         bot.reply_to(message, 'Я на сервере Heroku')
-    else:
-        bot.reply_to(message, 'Привет!')
+
 
 
 
