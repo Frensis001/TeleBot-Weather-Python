26d8e1cddfe2dd00e97b665734236627a0d266b6
Антон <Frensis001@gmail.com>
2022-10-05 16:57:27 +0500

    функция погоды ,

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import telebot
 import os
+from bot_pogoda import weather_api
+
 
 TOKEN_TELEBOT = os.environ["TOKEN_TELEBOT"]
 
@@ -8,9 +10,14 @@ bot = telebot.TeleBot(TOKEN_TELEBOT)
 
 @bot.message_handler(commands=['start'])
 def start_cmd(message):
-        bot.reply_to(message, 'Я на сервере Heroku')
-
-
-
+        user_id = message.from_user.id
+        user_name = message.from_user.first_name
+        bot.reply_to(message, 'Привет ' + user_name + "\nЭто телеграм бот погоды\nВведите город на Английском Пример:'Moscow'")
+        print(user_name, user_id,)
+        print(message)
+@bot.message_handler(func=lambda message: True)
+def weather_call(message):
+        Cyti = message.text.lower()
+        bot.reply_to(message, weather_api(Cyti))
 
 bot.polling()
---
+++ bot_pogoda.py
@@ -0,0 +1,18 @@
+import requests
+import os
+TOKEN_API_WEATHER = os.environ["TOKEN_API_WEATHER"]
+
+def weather_api(City):
+    url_api = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': City,
+        'appid': TOKEN_API_WEATHER,
+        'units': 'metric'
+
+    }
+
+    req = requests.get(url_api, params=params)
+    data = req.json()
+    print(data)
+    weather = str(data["main"]["temp"]) + '°C'
+    return weather
