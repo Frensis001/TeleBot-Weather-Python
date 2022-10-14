b0ce38e09de3de6d046f5c39904f2b38d626300f
Антон <Frensis001@gmail.com>
2022-10-14 21:11:29 +0500

    добавил влажность , ветер

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot(TOKEN_TELEBOT)
 @bot.message_handler(commands=['start'])
 def start_cmd(message):
         user_name = message.from_user.first_name
-        bot.send_message(message.from_user.id, 'Привет ' + user_name + "\nЭто телеграм бот погоды\nВведите город ")
+        bot.send_message(message.from_user.id, 'Приветствую вас ' + user_name + "\nЭто телеграм бот погоды\nВведите город ")
 
 
 @bot.message_handler(func=lambda message: True)
