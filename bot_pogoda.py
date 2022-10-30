9df3c708996895f3cbfd54425af77091dfb7f82a
frens <Frensis001@gmail.com>
2022-10-30 23:22:07 +0500

    добавил ,условие в России больше не работает Апи впн только через хироку , что усложняет работу

--- bot_pogoda.py
+++ bot_pogoda.py
@@ -20,7 +20,7 @@ def weather_api(City):
     if (data["cod"]) == '404':
         return "Есть такой город ?"
     else:
-        weather = "eeee" + str(data['snow']) +\
+        weather = "eeee" + str(data['weather']) +\
                   "\nТемпература  " + str(data["main"]["temp"]) + '°C ' + \
                   "\nВетер  " + str(data["wind"]["speed"]) + ' м/с' + \
                   "\nВлажность  " + str(data["main"]["humidity"]) + '%'
