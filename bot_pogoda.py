f2a92c77a9db6de6f5d170277109969c1aa16c97
frens <Frensis001@gmail.com>
2022-11-21 15:42:32 +0500

    Добавление проверки  if name == "__name__"

--- bot_pogoda.py
+++ bot_pogoda.py
@@ -41,14 +41,23 @@ def weather_api(City):
         time_set = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) + datetime.timedelta(seconds=time_zone)
         time_day = time_set - time_rise
         weather = str(data['weather'][0]['description']).capitalize() + \
-                "\n" + smail + \
-                "\nТемпература:  " + str(data["main"]["temp"]) + '°C ' + \
-                "\nВлажность:  " + str(data["main"]["humidity"]) + '%' + \
-                "\nАтмосферное давление: " + str(data["main"]['pressure']) + 'гПа' \
-                "\nСкорость ветера:  " + str(data["wind"]["speed"]) + ' м/с' + \
-                "\nОблачность: " + str(data["clouds"]['all']) + '%' + \
-                "\nВосход сольца: " + str(time_rise) + \
-                "\nЗакат солнца: " + str(time_set) + \
-                "\nПродолжительность дня: " + str(time_day)
+                  "\n" + smail + \
+                  "\nТемпература:  " + str(data["main"]["temp"]) + '°C ' + \
+                  "\nВлажность:  " + str(data["main"]["humidity"]) + '%' + \
+                  "\nАтмосферное давление: " + str(data["main"]['pressure']) + 'гПа' \
+                                                                               "\nСкорость ветера:  " + str(
+            data["wind"]["speed"]) + ' м/с' + \
+                  "\nОблачность: " + str(data["clouds"]['all']) + '%' + \
+                  "\nВосход сольца: " + str(time_rise) + \
+                  "\nЗакат солнца: " + str(time_set) + \
+                  "\nПродолжительность дня: " + str(time_day)
 
         return weather
+
+
+def main():
+    weather_api()
+
+
+if __name__ == "__main__":
+    main()
